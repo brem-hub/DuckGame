9fab5220dbd74be55902962bd8da49c9f599783b
Brem <65348875+brem-hub@users.noreply.github.com>
2020-08-07 13:32:32 +0300

    Added movement

---
+++ Scripts/MainPlayer.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+
+const MOVE_SPEED = 300
+
+func _physics_process(delta):
+	var move_vec = Vector2()
+	if Input.is_action_pressed("move_up"):
+		move_vec.y -= 1
+	if Input.is_action_pressed("move_down"):
+		move_vec.y += 1
+	if Input.is_action_pressed("move_left"):
+		move_vec.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_vec.y += 1
+	move_vec = move_vec.normalized()
+	move_and_collide(move_vec * MOVE_SPEED * delta)
