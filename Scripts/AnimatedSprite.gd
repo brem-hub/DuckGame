b74a55de9561e9e7582564f02f17247e7c64576f
FriTooTheEnd <69299145+FriTooTheEnd@users.noreply.github.com>
2020-08-08 19:19:27 -0500

    Trying to get this to work

---
+++ Scripts/AnimatedSprite.gd
@@ -0,0 +1,11 @@
+extends AnimatedSprite
+
+#Script to slow down animation when in slow mode
+onready var root = get_tree().get_root().get_child(0)
+onready var player = root.get_node("Player")
+
+func _physics_process(delta):
+	if player.slow_down:
+		speed_scale = 0.5
+	else:
+		speed_scale = 1
---
+++ Scripts/Player Scripts/Camera2D.gd
@@ -0,0 +1,28 @@
+extends Camera2D
+
+onready var root = get_tree().get_root().get_child(0)
+onready var player = get_parent()
+
+var distance_from_player = 400
+var shake_multiplier = 5
+var paused = false
+var victory = false
+
+func _ready():
+	set_as_toplevel(true)
+
+func _physics_process(delta):
+	if victory:	return
+	position.x = player.position.x + distance_from_player
+	position.y = 0
+	if !$Timer.is_stopped() && !paused:
+		position.x += rand_range(-shake_multiplier, shake_multiplier)
+		position.y += rand_range(-shake_multiplier, shake_multiplier)
+	if Input.is_action_just_pressed("pause") && player.health != 0:
+		_pause()
+
+func _pause():
+	paused = !paused
+	$Timer.paused = paused
+	get_tree().paused = paused
+	$Pause.visible = paused
